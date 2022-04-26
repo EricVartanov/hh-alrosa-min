d0172a380ec11349efaccf3f08be7bf63c75a2d6
EricVartanov <82024638+EricVartanov@users.noreply.github.com>
2022-04-26 12:59:17 +0300

    Add files via upload

---
+++ js/swiper.js
@@ -0,0 +1,13 @@
+const swiper1 = new Swiper(".idc-swiper", {
+    // Default parameters
+    loop: true,
+    direction: "horizontal",
+    slidesPerView: 'auto',
+    spaceBetween: 0,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    touchReleaseOnEdges: true,
+    allowTouchMove: true,
+    watchOverflow: true,
+    grabCursor: true,
+});
